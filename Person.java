1cf863d6c3cdfe9b8e914f0e4cad0c52710584fd
Tebong <nditebong@yahoo.co.uk>
2018-10-24 13:42:35 +0200

    setter and getter mothods

--- Person.java
+++ Person.java
@@ -5,7 +5,29 @@ public class Person {
 	private String name;
 	private int age;
 	private int idNumber;
-	private char gender;
+	private char gender
+	public string getName() {
+		return this.name;
+		public void setaName(String name);
+		this .name = name;
+		public int getAge() {
+			return this.age;
+			public void setAge(int age);
+			this.age = age;
+			public int getIdNumber() {
+				return this.idnumber;
+				public void setIdNumber(int idnumber);
+				this.idnumber = idnumber;
+				public char gender() {
+					return this.gender;
+					public void setGender(String f);
+					this.gender = gender;
+				}
+			}
+		
+		}
+		
+	}
 	
 	
 
