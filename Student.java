1b6b1026782d79d803469997532fd16e90b2a5b2
kehbila Leonard <kehbilatita@yahoo.com>
2018-10-24 13:50:22 +0200

    Systems print out x4

--- Student.java
+++ Student.java
@@ -1,10 +1,33 @@
 package examSolution.co.za;
 import java.util.Scanner;
 public class Student extends Person {
-	int studentNumber;
+	
 
 	public static void main(String[] args) {
+		
+		int studentNumber;
+		
+		Person p = new Person();
+		
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Full Names");
+		name = sc.nextInt(),
+				
+		System.out.println("Enter your age");
+		age = sc.nextInt();
+		
+		System.out.println("Enter idNumber");
+		idNumber = sc.nextInt();
+		
+		System.out.println("Enter student number");
+		studentNumber = sc.nextInt();
+		
+				
+				
+				
+				
+		
+		
 
 	}
 
