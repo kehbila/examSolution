7e64040390f849ec24ac87a5eb8da16af3f4bf34
Julius Ashu Agbor <asajtechnologies@gmail.com>
2018-10-25 07:15:39 +0200

    variable created

---
+++ Exam.java
@@ -0,0 +1,12 @@
+public class Exam{
+	private String name = "Java";
+	private String type = "multipleChoice";
+	private String duration = "1Hour";
+	
+	
+	
+	Public static void main(String [] args){
+		
+	}
+	
+}
