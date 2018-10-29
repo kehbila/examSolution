813970f502df5663524e40839820bd4a72ae316c
Julius Ashu Agbor <asajtechnologies@gmail.com>
2018-10-29 12:20:46 +0200

    added the import class

--- Person.java
+++ Person.java
@@ -5,30 +5,31 @@ public class Person {
 	private String name;
 	private int age;
 	private int idNumber;
-	private char gender
+	private char gender;
+	
 	public string getName() {
 		return this.name;
-		public void setaName(String name);
-		this .name = name;
+	}
+		public void setName(String name);{
+		this.name = name;
+		}
 		public int getAge() {
 			return this.age;
-			public void setAge(int age);
+		}
+			public void setAge(int age);{
 			this.age = age;
+			}
 			public int getIdNumber() {
 				return this.idnumber;
-				public void setIdNumber(int idnumber);
+			}
+				public void setIdNumber(int idnumber);{
 				this.idnumber = idnumber;
+				}
 				public char gender() {
 					return this.gender;
-					public void setGender(String f);
+				}
+					public void setGender(char gender);{
 					this.gender = gender;
 				}
 			}
 		
-		}
-		
-	}
-	
-	
-
-}
